082f674af74ee505882f6980e39e8ad4be0afa0c
Frank Zheng <ztcfrank123@gmail.com>
2019-09-02 19:51:36 -0700

    Integrate custom JS functions

--- abacus-calculator/index.js
+++ abacus-calculator/index.js
@@ -1,3 +1,31 @@
 // Links compiled Purescript world to Javascript
 
-module.exports = require('./output/Main/index')
+const { Just } = require('./output/Data.Maybe')
+const { calculate: psCalculate } = require('./output/Main/index')
+const { Func } = require('./output/Abacus.ExprToken')
+
+function calculate(
+  str,
+  {
+    functions = [],
+    operators = [],
+    useDefaultFunctions = true,
+    useDefaultOperators = true
+  } = {}
+) {
+  return psCalculate({
+    funcs: functions.map(toPsFunction),
+    opers: operators,
+    useDefFuncs: useDefaultFunctions,
+    useDefOpers: useDefaultOperators
+  })(str).value0
+}
+
+function toPsFunction(func) {
+  const arity = func.length
+  const symbol = func.name
+  const exec = args => Just.create(func(...args))
+  return Func({ arity, symbol, exec })
+}
+
+module.exports = { calculate }
--- abacus-client/index.js
+++ abacus-client/index.js
@@ -1,4 +1,23 @@
+const readline = require('readline')
+
 const { calculate } = require('abacus-calculator')
 
-console.log(calculate({ funcs: [], opers: [], useDefFuncs: true, useDefOpers: true })("1+3"))
+const options = {
+  functions: [
+    function pythagorean(a, b) {
+      return Math.sqrt(a * a + b * b)
+    }
+  ]
+}
 
+const rl = readline.createInterface(process.stdin, process.stdout)
+
+rl.setPrompt('abacus> ')
+rl.prompt()
+rl.on('line', function(line) {
+  if (line === 'quit') rl.close()
+  console.log(calculate(line, options))
+  rl.prompt()
+}).on('close', function() {
+  process.exit(0)
+})
